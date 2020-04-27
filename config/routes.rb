1866678448f5aa38b4fd2db43c59cb554bb55ecc
misato.arai <we11.in.the.wasteland@gmail.com>
2020-04-27 23:03:20 +0900

    プルリク2回目（本ブランチ最新)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
- 	resources :users, only: [:new, :create, :show, :edit, :update]
+
+ 	resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   root to: "blogs#index"
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
+  
 end
